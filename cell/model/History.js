bbd7e2578fc6c2d59ccf4640268379bcef64cff1
GoshaZotov <Igor.Zotov@onlyoffice.com>
2016-05-18 13:31:34 +0300

    syntax error

--- cell/model/History.js
+++ cell/model/History.js
@@ -139,7 +139,7 @@ function (window, undefined) {
 	window['AscCH'].historyitem_AutoFilter_ChangeTableRef = 13;
 	window['AscCH'].historyitem_AutoFilter_ChangeTableName = 14;
 	window['AscCH'].historyitem_AutoFilter_ClearFilterColumn = 15;
-})(window);
+
 
 function CHistory()
 {
