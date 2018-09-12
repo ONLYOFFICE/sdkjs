2bc10d1b51ba8bede2722bad9030ee612de40201
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2018-09-12 13:18:28 +0300

    [se] Add functions
    Add getCommentsRange. Use getCommentsRange for deleteCommentsRange

--- cell/model/CellComment.js
+++ cell/model/CellComment.js
@@ -377,6 +377,17 @@ CCellCommentator.prototype.isLockedComment = function(oComment, callbackFunc) {
 	}
 };
 
+	CCellCommentator.prototype.getCommentsRange = function (range) {
+		var res = [];
+		var aComments = this.model.aComments;
+		for (var i = 0; i < aComments.length; ++i) {
+			var comment = aComments[i];
+			if (range.contains(comment.nCol, comment.nRow)) {
+				res.push(comment.clone());
+			}
+		}
+	};
+
 	CCellCommentator.prototype.moveRangeComments = function (rangeFrom, rangeTo, copy) {
 		if (rangeFrom && rangeTo) {
 			var colOffset = rangeTo.c1 - rangeFrom.c1;
@@ -419,21 +430,14 @@ CCellCommentator.prototype.isLockedComment = function(oComment, callbackFunc) {
 	};
 
 	CCellCommentator.prototype.deleteCommentsRange = function (range) {
-		if (range) {
-			var aCommentId = [], i;
-			var aComments = this.model.aComments;
-			for (i = 0; i < aComments.length; ++i) {
-				var comment = aComments[i];
-				if (range.contains(comment.nCol, comment.nRow)) {
-					aCommentId.push(comment.asc_getId());
-				}
-			}
-			History.StartTransaction();
-			for (i = 0; i < aCommentId.length; i++) {
-				this.removeComment(aCommentId[i]);
-			}
-			History.EndTransaction();
+		this._deleteCommentsRange(this.getCommentsRange(range));
+	};
+	CCellCommentator.prototype._deleteCommentsRange = function (comments) {
+		History.StartTransaction();
+		for (var i = 0; i < comments.length; ++i) {
+			this.removeComment(comments[i].asc_getId());
 		}
+		History.EndTransaction();
 	};
 
 	CCellCommentator.prototype.getCommentByXY = function (x, y) {
