cryptpad/onlyoffice-editor

commit a6995e80938bbff62dcec852039973a7ef7aa155

[de] For bug 40874 (#743)

common/apiBase.js
@@ -1061,10 +1061,16 @@
                 }
             }
         };
-		this.CoAuthoringApi.onExpiredToken = function() {
+		this.CoAuthoringApi.onExpiredToken = function(data) {
 			t.sync_EndAction(c_oAscAsyncActionType.BlockInteraction, c_oAscAsyncAction.Open);
-			t.VersionHistory = null;
-			t.sendEvent('asc_onExpiredToken');
+			if (t.VersionHistory && t.VersionHistory.isRequested) {
+				var error = AscCommon.getDisconnectErrorCode(t.isDocumentLoadComplete, data["code"]);
+				var level = t.isDocumentLoadComplete ? Asc.c_oAscError.Level.NoCritical : Asc.c_oAscError.Level.Critical;
+				t.sendEvent('asc_onError', error, level);
+			} else {
+				t.VersionHistory = null;
+				t.sendEvent('asc_onExpiredToken');
+			}
 		};
 		this.CoAuthoringApi.onHasForgotten = function() {
 			//todo very bad way, need rewrite
@@ -1080,24 +1086,22 @@
 		/**
 		 * Event об отсоединении от сервера
 		 * @param {jQuery} e  event об отсоединении с причиной
-		 * @param {Bool} isDisconnectAtAll  окончательно ли отсоединяемся(true) или будем пробовать сделать reconnect(false) + сами отключились
-		 * @param {Bool} isCloseCoAuthoring
+		 * @param {opt_closeCode: AscCommon.c_oCloseCode.drop} opt_closeCode
 		 */
-		this.CoAuthoringApi.onDisconnect = function(e, error)
+		this.CoAuthoringApi.onDisconnect = function(e, opt_closeCode)
 		{
 			if (AscCommon.ConnectionState.None === t.CoAuthoringApi.get_state())
 			{
 				t.asyncServerIdEndLoaded();
 			}
-			if (null != error)
-			{
-				t.setViewModeDisconnect();
-				if (Asc.c_oAscError.ID.UpdateVersion === error.code) {
-					t.sendEvent("asc_onDocumentUpdateVersion", function() {
-					});
-				} else {
-					t.sendEvent('asc_onError', error.code, error.level);
-				}
+			var error = AscCommon.getDisconnectErrorCode(t.isDocumentLoadComplete, opt_closeCode);
+			var level = t.isDocumentLoadComplete ? Asc.c_oAscError.Level.NoCritical : Asc.c_oAscError.Level.Critical;
+			t.setViewModeDisconnect();
+			if (Asc.c_oAscError.ID.UpdateVersion === error) {
+				t.sendEvent("asc_onDocumentUpdateVersion", function() {
+				});
+			} else {
+				t.sendEvent('asc_onError', error, level);
 			}
 		};
 		this.CoAuthoringApi.onDocumentOpen = function (inputWrap) {

common/docscoapi.js
@@ -123,8 +123,8 @@
       this._CoAuthoringApi.onLocksReleasedEnd = function() {
         t.callback_OnLocksReleasedEnd();
       };
-      this._CoAuthoringApi.onDisconnect = function(e, error) {
-        t.callback_OnDisconnect(e, error);
+      this._CoAuthoringApi.onDisconnect = function(e, code) {
+        t.callback_OnDisconnect(e, code);
       };
       this._CoAuthoringApi.onWarning = function(e) {
         t.callback_OnWarning(e);
@@ -463,11 +463,11 @@
   /**
    * Event об отсоединении от сервера
    * @param {jQuery} e  event об отсоединении с причиной
-   * @param {code: Asc.c_oAscError.ID, level: Asc.c_oAscError.Level} error
+   * @param {code: AscCommon.c_oCloseCode.drop} code
    */
-  CDocsCoApi.prototype.callback_OnDisconnect = function(e, error) {
+  CDocsCoApi.prototype.callback_OnDisconnect = function(e, code) {
     if (this.onDisconnect) {
-      this.onDisconnect(e, error);
+      this.onDisconnect(e, code);
     }
   };
 
@@ -1021,7 +1021,7 @@
 
   DocsCoApi.prototype._onExpiredToken = function(data) {
     if (this.onExpiredToken) {
-      this.onExpiredToken();
+      this.onExpiredToken(data);
     }
   };
 
@@ -1423,7 +1423,8 @@
 
   DocsCoApi.prototype._onDrop = function(data) {
     this.disconnect();
-    this.onDisconnect(data ? data['description'] : '', this._getDisconnectErrorCode(data && data['code']));
+    var code = data && data['code'] || c_oCloseCode.drop;
+    this.onDisconnect(data ? data['description'] : '', code);
   };
 
   DocsCoApi.prototype._onWarning = function(data) {
@@ -1732,7 +1733,7 @@
 				this._onRefreshToken(dataObject["messages"]);
 				break;
 			case 'expiredToken' :
-				this._onExpiredToken();
+				this._onExpiredToken(dataObject);
 				break;
 			case 'forceSaveStart' :
 				this._onForceSaveStart(dataObject["messages"]);
@@ -1755,13 +1756,11 @@
 		this._state = ConnectionState.Reconnect;
 		var bIsDisconnectAtAll = ((c_oCloseCode.serverShutdown <= evt.code && evt.code <= c_oCloseCode.drop) ||
 			this.attemptCount >= this.maxAttemptCount);
-		var error = null;
 		if (bIsDisconnectAtAll) {
 			this._state = ConnectionState.ClosedAll;
-			error = this._getDisconnectErrorCode(evt.code);
 		}
 		if (this.onDisconnect) {
-			this.onDisconnect(evt.reason, error);
+			this.onDisconnect(evt.reason, evt.code);
 		}
 		//Try reconect
 		if (!bIsDisconnectAtAll) {
@@ -1782,35 +1781,6 @@
 		this.reconnectTimeout = setTimeout(function () {
 			t._reconnect();
 		}, this.reconnectInterval);
-	};
-
-	DocsCoApi.prototype._getDisconnectErrorCode = function(opt_closeCode) {
-		var code = this.isCloseCoAuthoring ? Asc.c_oAscError.ID.UserDrop : Asc.c_oAscError.ID.CoAuthoringDisconnect;
-		var level = Asc.c_oAscError.Level.NoCritical;
-		if (c_oCloseCode.serverShutdown === opt_closeCode) {
-			code = Asc.c_oAscError.ID.CoAuthoringDisconnect;
-		} else if (c_oCloseCode.sessionIdle === opt_closeCode) {
-			code = Asc.c_oAscError.ID.SessionIdle;
-		} else if (c_oCloseCode.sessionAbsolute === opt_closeCode) {
-			code = Asc.c_oAscError.ID.SessionAbsolute;
-		} else if (c_oCloseCode.accessDeny === opt_closeCode) {
-			code = Asc.c_oAscError.ID.AccessDeny;
-		} else if (c_oCloseCode.jwtExpired === opt_closeCode) {
-			if (this.jwtSession) {
-				code = Asc.c_oAscError.ID.SessionToken;
-			} else {
-				code = Asc.c_oAscError.ID.KeyExpire;
-				level = Asc.c_oAscError.Level.Critical;
-			}
-		} else if (c_oCloseCode.jwtError === opt_closeCode) {
-			code = Asc.c_oAscError.ID.VKeyEncrypt;
-			level = Asc.c_oAscError.Level.Critical;
-		} else if (c_oCloseCode.drop === opt_closeCode) {
-			code = Asc.c_oAscError.ID.UserDrop;
-		} else if (c_oCloseCode.updateVersion === opt_closeCode) {
-			code = Asc.c_oAscError.ID.UpdateVersion;
-		}
-		return {code: code, level: level};
 	};
 
   //----------------------------------------------------------export----------------------------------------------------

common/docscoapicommon.js
@@ -153,6 +153,28 @@
 		Timeout: 2
 	};
 
+	function getDisconnectErrorCode (isDocumentLoadComplete, opt_closeCode) {
+		var code = Asc.c_oAscError.ID.CoAuthoringDisconnect;
+		if (c_oCloseCode.serverShutdown === opt_closeCode) {
+			code = Asc.c_oAscError.ID.CoAuthoringDisconnect;
+		} else if (c_oCloseCode.sessionIdle === opt_closeCode) {
+			code = Asc.c_oAscError.ID.SessionIdle;
+		} else if (c_oCloseCode.sessionAbsolute === opt_closeCode) {
+			code = Asc.c_oAscError.ID.SessionAbsolute;
+		} else if (c_oCloseCode.accessDeny === opt_closeCode) {
+			code = Asc.c_oAscError.ID.AccessDeny;
+		} else if (c_oCloseCode.jwtExpired === opt_closeCode) {
+			code = isDocumentLoadComplete ? Asc.c_oAscError.ID.SessionToken : Asc.c_oAscError.ID.KeyExpire;
+		} else if (c_oCloseCode.jwtError === opt_closeCode) {
+			code = Asc.c_oAscError.ID.VKeyEncrypt;
+		} else if (c_oCloseCode.drop === opt_closeCode) {
+			code = Asc.c_oAscError.ID.UserDrop;
+		} else if (c_oCloseCode.updateVersion === opt_closeCode) {
+			code = Asc.c_oAscError.ID.UpdateVersion;
+		}
+		return code;
+	}
+
   /*
    * Export
    * -----------------------------------------------------------------------------
@@ -167,6 +189,8 @@
   prot["asc_getState"] = prot.asc_getState;
   prot["asc_getColor"] = prot.asc_getColor;
   prot["asc_getView"] = prot.asc_getView;
+
+  window["AscCommon"].getDisconnectErrorCode = getDisconnectErrorCode;
 
   window["AscCommon"].ConnectionState = ConnectionState;
   window["AscCommon"].c_oEditorId = c_oEditorId;

ONLYOFFICE/sdkjs

commit b56b9c72cfb0505a69029ab813a133d5a1820a16

[all] Fix problem with disconnect error introduced in a6995e80938bbff62dcec852039973a7ef7aa155

common/apiBase.js
@@ -1094,14 +1094,16 @@
 			{
 				t.asyncServerIdEndLoaded();
 			}
-			var error = AscCommon.getDisconnectErrorCode(t.isDocumentLoadComplete, opt_closeCode);
-			var level = t.isDocumentLoadComplete ? Asc.c_oAscError.Level.NoCritical : Asc.c_oAscError.Level.Critical;
-			t.setViewModeDisconnect();
-			if (Asc.c_oAscError.ID.UpdateVersion === error) {
-				t.sendEvent("asc_onDocumentUpdateVersion", function() {
-				});
-			} else {
-				t.sendEvent('asc_onError', error, level);
+			if (null != opt_closeCode) {
+				var error = AscCommon.getDisconnectErrorCode(t.isDocumentLoadComplete, opt_closeCode);
+				var level = t.isDocumentLoadComplete ? Asc.c_oAscError.Level.NoCritical : Asc.c_oAscError.Level.Critical;
+				t.setViewModeDisconnect();
+				if (Asc.c_oAscError.ID.UpdateVersion === error) {
+					t.sendEvent("asc_onDocumentUpdateVersion", function() {
+					});
+				} else {
+					t.sendEvent('asc_onError', error, level);
+				}
 			}
 		};
 		this.CoAuthoringApi.onDocumentOpen = function (inputWrap) {

common/docscoapi.js
@@ -1756,11 +1756,13 @@
 		this._state = ConnectionState.Reconnect;
 		var bIsDisconnectAtAll = ((c_oCloseCode.serverShutdown <= evt.code && evt.code <= c_oCloseCode.drop) ||
 			this.attemptCount >= this.maxAttemptCount);
+		var code = null;
 		if (bIsDisconnectAtAll) {
 			this._state = ConnectionState.ClosedAll;
+			code = evt.code;
 		}
 		if (this.onDisconnect) {
-			this.onDisconnect(evt.reason, evt.code);
+			this.onDisconnect(evt.reason, code);
 		}
 		//Try reconect
 		if (!bIsDisconnectAtAll) {