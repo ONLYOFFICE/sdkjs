c74385c20994cc19d3bb5405de049e852e434830
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2020-01-29 18:24:17 +0300

    Remove second parameter

--- common/Drawings/Format/GraphicObjectBase.js
+++ common/Drawings/Format/GraphicObjectBase.js
@@ -826,7 +826,7 @@
     CGraphicObjectBase.prototype.checkTypeCorrect = function(){
         return true;
     };
-    CGraphicObjectBase.prototype.handleUpdateExtents = function(bExtX, bCell){
+    CGraphicObjectBase.prototype.handleUpdateExtents = function(bExtX){
     };
     CGraphicObjectBase.prototype.handleUpdatePosition = function(){
     };
