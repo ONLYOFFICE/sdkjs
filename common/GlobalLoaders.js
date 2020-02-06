cryptpad/onlyoffice-editor

commit 51bb64db619eecfda4d6e34436f411ea5932b58f

[all] Fix bug 44157 (#786)
Add retry for image requests and loadScript

common/GlobalLoaders.js
@@ -609,6 +609,9 @@
 						oThis._LoadImages();
 					}
 				};
+				AscCommon.backoffOnErrorImg(oImage.Image, function() {
+					oThis.loadImageByUrl(oImage.Image, oImage.src);
+				});
 				//oImage.Image.crossOrigin = 'anonymous';
 				oThis.loadImageByUrl(oImage.Image, oImage.src);
 
@@ -640,6 +643,9 @@
                 oImage.Status = ImageLoadStatus.Complete;
                 oThis.Api.asyncImageEndLoaded(oImage);
             };
+            AscCommon.backoffOnErrorImg(oImage.Image, function() {
+                oThis.loadImageByUrl(oImage.Image, oImage.src);
+            });
             //oImage.Image.crossOrigin = 'anonymous';
             this.loadImageByUrl(oImage.Image, oImage.src);
             return null;
@@ -661,6 +667,9 @@
                 oImage.Image = null;
                 oThis.Api.asyncImageEndLoadedBackground(oImage);
             };
+            AscCommon.backoffOnErrorImg(oImage.Image, function() {
+                oThis.loadImageByUrl(oImage.Image, oImage.src);
+            });
             //oImage.Image.crossOrigin = 'anonymous';
             oThis.loadImageByUrl(oImage.Image, oImage.src);
         };
@@ -711,6 +720,9 @@
 					if (oThis.loadImageCallBackCounter == oThis.loadImageCallBackCounterMax)
 						oThis.LoadImagesWithCallbackEnd();
 				};
+				AscCommon.backoffOnErrorImg(oImage.Image, function() {
+					oThis.loadImageByUrl(oImage.Image, oImage.src);
+				});
 				//oImage.Image.crossOrigin = 'anonymous';
                 this.loadImageByUrl(oImage.Image, oImage.src);
 			}

common/editorscommon.js
@@ -3784,11 +3784,20 @@
 				return;
 		}
 
+		var backoff = new AscCommon.Backoff(AscCommon.g_oBackoffDefaults);
+		loadScriptWithBackoff(backoff, url, onSuccess, onError);
+	}
+
+	function loadScriptWithBackoff(backoff, url, onSuccess, onError){
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = url;
 		script.onload = onSuccess;
-		script.onerror = onError;
+		script.onerror = function() {
+			backoff.attempt(onError, function() {
+				loadScriptBackoff(backoff, url, onSuccess, onError);
+			});
+		};
 
 		// Fire the loading
 		document.head.appendChild(script);
@@ -4976,6 +4985,75 @@
 		return null;
 	}
 
+	var g_oBackoffDefaults = {
+		retries: 2,
+		factor: 2,
+		minTimeout: 100,
+		maxTimeout: 2000,
+		randomize: true
+	};
+
+	function Backoff(opts) {
+		this.attempts = 0;
+		this.opts = opts;
+	}
+
+	Backoff.prototype.attempt = function(fError, fRetry) {
+		var timeout = this.nextTimeout();
+		if (timeout > 0) {
+			setTimeout(function() {
+				fRetry();
+			}, timeout);
+		} else {
+			fError();
+		}
+	};
+	Backoff.prototype.nextTimeout = function() {
+		var timeout = -1;
+		if (this.attempts < this.opts.retries) {
+			timeout = this.createTimeout(this.attempts, this.opts);
+			this.attempts++;
+		}
+		return timeout;
+	};
+	Backoff.prototype.createTimeout = function(attempt, opts) {
+		//like npm retry
+		var random = (opts.randomize)
+			? (Math.random() + 1)
+			: 1;
+
+		var timeout = Math.round(random * opts.minTimeout * Math.pow(opts.factor, attempt));
+		timeout = Math.min(timeout, opts.maxTimeout);
+
+		return timeout;
+	};
+	function backoffOnError(obj, onError, onRetry) {
+		if (!onRetry) {
+			return onError;
+		}
+		var backoff = new Backoff(g_oBackoffDefaults);
+		return function() {
+			var timeout = backoff.nextTimeout();
+			if (timeout > 0) {
+				setTimeout(function() {
+					onRetry.apply(obj, arguments);
+				}, timeout);
+			} else if (onError) {
+				onError.apply(obj, arguments);
+			}
+		};
+	}
+	function backoffOnErrorImg(img, onRetry) {
+		//$self.attr("src", $self.attr("src"));
+		//https://github.com/doomhz/jQuery-Image-Reloader/blob/dd1f626b25628ede498ae2489a0c2963f1c3cf61/jquery.imageReloader.js#L56
+		if (!onRetry) {
+			onRetry = function() {
+				img.setAttribute('src', img.getAttribute('src'));
+			};
+		}
+		img.onerror = backoffOnError(img, img.onerror, onRetry);
+	}
+
 	//------------------------------------------------------------export---------------------------------------------------
 	window['AscCommon'] = window['AscCommon'] || {};
 	window["AscCommon"].getSockJs = getSockJs;
@@ -5063,6 +5141,10 @@
 	window["AscCommon"].g_oIdCounter = g_oIdCounter;
 
 	window["AscCommon"].g_oHtmlCursor = g_oHtmlCursor;
+
+	window["AscCommon"].g_oBackoffDefaults = g_oBackoffDefaults;
+	window["AscCommon"].Backoff = Backoff;
+	window["AscCommon"].backoffOnErrorImg = backoffOnErrorImg;
 
 	window["AscCommon"].CSignatureDrawer = window["AscCommon"]["CSignatureDrawer"] = CSignatureDrawer;
 	var prot = CSignatureDrawer.prototype;

word/Drawing/DrawingDocument.js
@@ -131,10 +131,12 @@ function CTableOutlineDr()
 	this.image = new Image();
 	this.image.src = "../../../../sdkjs/common/Images/table_move.png";
 	this.image.onload = function() { this.asc_complete = true; };
+	AscCommon.backoffOnErrorImg(this.image);
 
 	this.image2 = new Image();
 	this.image2.src = "../../../../sdkjs/common/Images/table_move_2x.png";
 	this.image2.onload = function() { this.asc_complete = true; };
+	AscCommon.backoffOnErrorImg(this.image2);
 
 	this.TableOutline = null;
 	this.Counter = 0;

word/Drawing/buttons.js
@@ -77,20 +77,24 @@
                 this.images[0] = new Image();
                 this.images[0].onload = function() { this.asc_complete = true; };
                 this.images[0].src = "../../../../sdkjs/common/Images/placeholders/" + url + ".png";
+                AscCommon.backoffOnErrorImg(this.images[0]);
 
                 this.images[1] = new Image();
                 this.images[1].onload = function() { this.asc_complete = true; };
                 this.images[1].src = "../../../../sdkjs/common/Images/placeholders/" + url + "@2x.png";
+                AscCommon.backoffOnErrorImg(this.images[1]);
             };
             this.loadActive = function(url)
             {
                 this.images[2] = new Image();
                 this.images[2].onload = function() { this.asc_complete = true; };
                 this.images[2].src = "../../../../sdkjs/common/Images/placeholders/" + url + "_active.png";
+                AscCommon.backoffOnErrorImg(this.images[2]);
 
                 this.images[3] = new Image();
                 this.images[3].onload = function() { this.asc_complete = true; };
                 this.images[3].src = "../../../../sdkjs/common/Images/placeholders/" + url + "_active@2x.png";
+                AscCommon.backoffOnErrorImg(this.images[3]);
             };
             this.get = function()
             {
@@ -632,18 +636,22 @@
                 this.images[0] = new Image();
                 this.images[0].onload = function() { this.asc_complete = true; };
                 this.images[0].src = "../../../../sdkjs/common/Images/content_controls/" + url + ".png";
+                AscCommon.backoffOnErrorImg(this.images[0]);
 
                 this.images[1] = new Image();
                 this.images[1].onload = function() { this.asc_complete = true; };
                 this.images[1].src = "../../../../sdkjs/common/Images/content_controls/" + url + "_active.png";
+                AscCommon.backoffOnErrorImg(this.images[1]);
 
                 this.images[2] = new Image();
                 this.images[2].onload = function() { this.asc_complete = true; };
                 this.images[2].src = "../../../../sdkjs/common/Images/content_controls/" + url + "@2x.png";
+                AscCommon.backoffOnErrorImg(this.images[2]);
 
                 this.images[3] = new Image();
                 this.images[3].onload = function() { this.asc_complete = true; };
                 this.images[3].src = "../../../../sdkjs/common/Images/content_controls/" + url + "_active@2x.png";
+                AscCommon.backoffOnErrorImg(this.images[3]);
             };
 
             this.get = function(isActive)

ONLYOFFICE/sdkjs

commit 81954da000e4a745f55ba8f57a671ed99609d5ff

[all] Fix error with pasting from word introduced in 51bb64db619eecfda4d6e34436f411ea5932b58f

common/GlobalLoaders.js
@@ -609,8 +609,8 @@
 						oThis._LoadImages();
 					}
 				};
-				AscCommon.backoffOnErrorImg(oImage.Image, function() {
-					oThis.loadImageByUrl(oImage.Image, oImage.src);
+				AscCommon.backoffOnErrorImg(oImage.Image, function(img) {
+					oThis.loadImageByUrl(img, img.src);
 				});
 				//oImage.Image.crossOrigin = 'anonymous';
 				oThis.loadImageByUrl(oImage.Image, oImage.src);
@@ -643,8 +643,8 @@
                 oImage.Status = ImageLoadStatus.Complete;
                 oThis.Api.asyncImageEndLoaded(oImage);
             };
-            AscCommon.backoffOnErrorImg(oImage.Image, function() {
-                oThis.loadImageByUrl(oImage.Image, oImage.src);
+            AscCommon.backoffOnErrorImg(oImage.Image, function(img) {
+                oThis.loadImageByUrl(img, img.src);
             });
             //oImage.Image.crossOrigin = 'anonymous';
             this.loadImageByUrl(oImage.Image, oImage.src);
@@ -667,8 +667,8 @@
                 oImage.Image = null;
                 oThis.Api.asyncImageEndLoadedBackground(oImage);
             };
-            AscCommon.backoffOnErrorImg(oImage.Image, function() {
-                oThis.loadImageByUrl(oImage.Image, oImage.src);
+            AscCommon.backoffOnErrorImg(oImage.Image, function(img) {
+                oThis.loadImageByUrl(img, img.src);
             });
             //oImage.Image.crossOrigin = 'anonymous';
             oThis.loadImageByUrl(oImage.Image, oImage.src);
@@ -720,8 +720,8 @@
 					if (oThis.loadImageCallBackCounter == oThis.loadImageCallBackCounterMax)
 						oThis.LoadImagesWithCallbackEnd();
 				};
-				AscCommon.backoffOnErrorImg(oImage.Image, function() {
-					oThis.loadImageByUrl(oImage.Image, oImage.src);
+				AscCommon.backoffOnErrorImg(oImage.Image, function(img) {
+					oThis.loadImageByUrl(img, img.src);
 				});
 				//oImage.Image.crossOrigin = 'anonymous';
                 this.loadImageByUrl(oImage.Image, oImage.src);

common/editorscommon.js
@@ -5036,7 +5036,7 @@
 			var timeout = backoff.nextTimeout();
 			if (timeout > 0) {
 				setTimeout(function() {
-					onRetry.apply(obj, arguments);
+					onRetry.call(obj, obj);
 				}, timeout);
 			} else if (onError) {
 				onError.apply(obj, arguments);
@@ -5047,7 +5047,7 @@
 		//$self.attr("src", $self.attr("src"));
 		//https://github.com/doomhz/jQuery-Image-Reloader/blob/dd1f626b25628ede498ae2489a0c2963f1c3cf61/jquery.imageReloader.js#L56
 		if (!onRetry) {
-			onRetry = function() {
+			onRetry = function(img) {
 				img.setAttribute('src', img.getAttribute('src'));
 			};
 		}