f447bd8dec9527b34db3897f2d13c65a007a60d2
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2020-05-27 12:53:32 +0300

    Fix some problems with working of text forms

--- word/Editor/Document.js
+++ word/Editor/Document.js
@@ -3047,6 +3047,9 @@ CDocument.prototype.private_FinalizeFormChange = function()
 		var oForm = this.Action.Additional.FormChange[sKey].Form;
 		var oPr   = this.Action.Additional.FormChange[sKey].Pr;
 
+		if (!oForm.IsUseInDocument())
+			continue;
+
 		if (oForm.IsCheckBox())
 		{
 			var isChecked = oForm.GetCheckBoxPr().Checked;
@@ -3055,6 +3058,9 @@ CDocument.prototype.private_FinalizeFormChange = function()
 				for (var sId in this.SpecialForms)
 				{
 					var oTempForm = this.SpecialForms[sId];
+					if (!oTempForm.IsUseInDocument())
+						continue;
+
 					if (oTempForm !== oForm && oTempForm.IsRadioButton() && sKey === oTempForm.GetCheckBoxPr().GroupKey)
 					{
 						if (oTempForm.GetCheckBoxPr().Checked)
@@ -3067,6 +3073,9 @@ CDocument.prototype.private_FinalizeFormChange = function()
 				for (var sId in this.SpecialForms)
 				{
 					var oTempForm = this.SpecialForms[sId];
+					if (!oTempForm.IsUseInDocument())
+						continue;
+
 					if (oTempForm !== oForm && oTempForm.IsCheckBox() && (!oTempForm.IsRadioButton()) && sKey === oTempForm.GetFormKey())
 					{
 						if (isChecked !== oTempForm.GetCheckBoxPr().Checked)
@@ -3080,6 +3089,8 @@ CDocument.prototype.private_FinalizeFormChange = function()
 			for (var sId in this.SpecialForms)
 			{
 				var oTempForm = this.SpecialForms[sId];
+				if (!oTempForm.IsUseInDocument())
+					continue;
 
 				if (oTempForm !== oForm && sKey === oTempForm.GetFormKey() && oTempForm.IsPicture())
 				{
@@ -3103,7 +3114,7 @@ CDocument.prototype.private_FinalizeFormChange = function()
 			{
 				var oTempForm = this.SpecialForms[sId];
 
-				if (oTempForm.IsPicture() || oTempForm.IsCheckBox())
+				if (!oTempForm.IsUseInDocument() || oTempForm.IsPicture() || oTempForm.IsCheckBox())
 					continue;
 
 				if (oTempForm !== oForm && sKey === oTempForm.GetFormKey())
@@ -15148,7 +15159,15 @@ CDocument.prototype.AddContentControlTextForm = function(oPr)
 
 	if (oPr.Comb)
 	{
-		var oDocPart = oCC.SetPlaceholderText(String.fromCharCode(oPr.CombPlaceholderSymbol));
+		if (!oPr.MaxCharacters || !oPr.MaxCharacters <= 0)
+			oPr.MaxCharacters = 12;
+
+		var oDocPart
+		if (oPr.CombPlaceholderSymbol)
+			oCC.SetPlaceholderText(String.fromCharCode(oPr.CombPlaceholderSymbol));
+		else
+			oCC.SetPlaceholder(c_oAscDefaultPlaceholderName.TextForm);
+
 		if (oDocPart && oPr.CombPlaceholderFont)
 		{
 			oDocPart.SelectAll();
