3476bb853cfbe96e4f047dae86f7cf4f1feaba81
GoshaZotov <Igor.Zotov@onlyoffice.com>
2020-06-15 18:38:39 +0300

    [se] Add tests

--- cell/.unit-tests/FormulaTests.js
+++ cell/.unit-tests/FormulaTests.js
@@ -13037,5 +13037,82 @@ $( function () {
 		testArrayFormula2("ADDRESS", 2, 5);
 	} );
 
+	test( "Test: \"reference argument test\"", function () {
+		ws.getRange2( "A1" ).setValue( "1" );
+		ws.getRange2( "A2" ).setValue( "2" );
+		ws.getRange2( "A3" ).setValue( "3" );
+		ws.getRange2( "A4" ).setValue( "4" );
+		ws.getRange2( "A5" ).setValue( "5" );
+		ws.getRange2( "A6" ).setValue( "6" );
+
+		ws.getRange2( "B1" ).setValue( "2" );
+		ws.getRange2( "B2" ).setValue( "" );
+		ws.getRange2( "B3" ).setValue( "3" );
+		ws.getRange2( "B4" ).setValue( "4" );
+		ws.getRange2( "B5" ).setValue( "5" );
+		ws.getRange2( "B6" ).setValue( "6" );
+
+		/*oParser = new parserFormula( 'IRR(SIN(A1:B4))', 'A2', ws );
+		ok( oParser.parse(),'IRR(SIN(A1:B4))' );
+		strictEqual( oParser.calculate().getValue().toFixed(9) - 0,-0.290303641,'IRR(SIN(A1:B4))');
+
+		oParser = new parserFormula( 'MIRR(SIN(A2:B4),1,1)', 'A2', ws );
+		ok( oParser.parse(),'MIRR(SIN(A2:B4),1,1)' );
+		strictEqual( oParser.calculate().getValue(),3.11187861965467,'MIRR(SIN(A2:B4),1,1)');*/
+
+		oParser = new parserFormula( 'COLUMN(INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'COLUMN(INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'COLUMN(INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'COLUMNS(SIN($A$1:$B$4))', 'A2', ws );
+		ok( oParser.parse(),'COLUMNS(SIN($A$1:$B$4))' );
+		strictEqual( oParser.calculate().getValue(),2,'COLUMNS(SIN($A$1:$B$4))');
+
+		oParser = new parserFormula( 'INDEX(SIN(A1:B3),1,1)', 'A2', ws );
+		ok( oParser.parse(),'INDEX(SIN(A1:B3),1,1)' );
+		strictEqual( oParser.calculate().getValue().toFixed(9) - 0,0.841470985,'INDEX(SIN(A1:B3),1,1)');
+
+		/*oParser = new parserFormula( 'OFFSET(INDEX(A1:B3,1,1),1,1)', 'A2', ws );
+		ok( oParser.parse(),'OFFSET(INDEX(A1:B3,1,1),1,1)' );
+		strictEqual( oParser.calculate().getValue(),0,'OFFSET(INDEX(A1:B3,1,1),1,1)');*/
+
+		oParser = new parserFormula( 'ROW(INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'ROW(INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'ROW(INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'ROWS(SIN(A1:B3))', 'A2', ws );
+		ok( oParser.parse(),'ROWS(SIN(A1:B3))' );
+		strictEqual( oParser.calculate().getValue(),3,'ROWS(SIN(A1:B3))');
+
+		oParser = new parserFormula( 'SUBTOTAL(1,INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'SUBTOTAL(1,INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'SUBTOTAL(1,INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'SUMIF(INDEX(A1:B3,1,1),1,INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'SUMIF(INDEX(A1:B3,1,1),1,INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'SUMIF(INDEX(A1:B3,1,1),1,INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'SUMIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'SUMIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'SUMIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'AVERAGEIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'AVERAGEIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'AVERAGEIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'COUNTBLANK(INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'COUNTBLANK(INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),0,'COUNTBLANK(INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'COUNTIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'COUNTIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'COUNTIF(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))');
+
+		oParser = new parserFormula( 'COUNTIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))', 'A2', ws );
+		ok( oParser.parse(),'COUNTIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))' );
+		strictEqual( oParser.calculate().getValue(),1,'COUNTIFS(INDEX(A1:B3,1,1),INDEX(A1:B3,1,1))');
+
+	} );
+
 	wb.dependencyFormulas.unlockRecal();
 } );
