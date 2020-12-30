e52f1304061d4dbb389b1475bd8b735043bf0945
GoshaZotov <Igor.Zotov@onlyoffice.com>
2020-12-30 18:28:14 +0300

    [se] Fix bug 48166

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -14798,8 +14798,12 @@
 					}
 				}
 
-				var aDataValidations = wsFrom._getCopyDataValidationByRange(from);
-				if(aDataValidations.length > 0) {
+
+				var aDataValidations;
+				if (wsFrom.dataValidations) {
+					aDataValidations = wsFrom.dataValidations.getIntersectionByRange(from);
+				}
+				if(aDataValidations && aDataValidations.length > 0) {
 					var newDataValidations = [];
 					for (var i = to.c1; i <= to.c2; i += nDx) {
 						for (var j = to.r1; j <= to.r2; j += nDy) {
@@ -14809,8 +14813,24 @@
 								for (var n = 0; n < ranges.length; n++) {
 									var _newRange = new Asc.Range(i + ranges[n].c1 - from.c1, j + ranges[n].r1 - from.r1, i + ranges[n].c2 - from.c1, j + ranges[n].r2 - from.r1);
 									if (to.containsRange(_newRange)) {
+										if (!newDataValidations[k]) {
+											newDataValidations[k] = [];
+										}
 										newDataValidations[k].push(_newRange);
 									}
+								}
+							}
+						}
+					}
+					if (newDataValidations && newDataValidations.length) {
+						for (var i = 0; i < newDataValidations.length; i++) {
+							if (newDataValidations[i] && newDataValidations[i].length) {
+								var fromDataValidation = wsFrom.getDataValidationById(aDataValidations[i].id);
+								if (fromDataValidation) {
+									fromDataValidation = fromDataValidation.data;
+									var toDataValidation = fromDataValidation.clone();
+									toDataValidation.ranges = fromDataValidation.ranges.concat(newDataValidations[i]);
+									wsTo.dataValidations.change(wsTo, fromDataValidation, toDataValidation, true);
 								}
 							}
 						}
