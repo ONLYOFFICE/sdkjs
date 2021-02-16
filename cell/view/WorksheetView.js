639d884539bf8c9b9b2e144a53a8bd267acd437c
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2021-02-16 20:47:31 +0300

    [se] Fix bug 48732

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -20590,7 +20590,7 @@
 				t._isLockedDefNames(callback);
 			});
 		} else {
-			this._isLockedCells(lockRanges, null, function(success) {
+			var callbackLockCell = function (success) {
 				if (!success) {
 					History.EndTransaction();
 					return;
@@ -20607,7 +20607,12 @@
 				} else {
 					t._isLockedDefNames(callback);
 				}
-			});
+			};
+			if(lockRanges.length > 0) {
+				this._isLockedCells(lockRanges, null, callbackLockCell);
+			} else {
+				callbackLockCell(true);
+			}
 		}
 	};
 
