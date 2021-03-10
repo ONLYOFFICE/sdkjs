2c5d3bbf70548c51d5d4b8ad336d186ca5243e6d
GoshaZotov <Igor.Zotov@onlyoffice.com>
2021-03-10 13:16:28 +0300

    [se] Set all presets

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -21209,31 +21209,50 @@
 			History.EndTransaction();
 		};
 
-		var nActive = this.model.workbook.nActive;
-		var i;
-		if (presetId !== undefined) {
-			var _generatedRule = t.model.generateCFRuleFromPreset(presetId);
-			if (_generatedRule) {
-				_generatedRule.priority = 1;
+		var _checkRule = function (_rule) {
+			if (_rule) {
 				if (!arr) {
 					arr = [];
 				}
 				if (!arr[nActive]) {
 					arr[nActive] = [];
 				}
-				arr[nActive].push(_generatedRule);
+				arr[nActive].push(_rule);
 
-				//двигаем приоритет у всех остальных и добавляем их в список измененных
-				if (t.model.aConditionalFormattingRules) {
-					for (i = 0; i < t.model.aConditionalFormattingRules.length; i++) {
-						var _id = t.model.aConditionalFormattingRules[i].id;
-						var oRule = t.model.aConditionalFormattingRules[i].clone();
-						oRule.id = _id;
-						oRule.priority++;
-						arr[nActive].push(oRule);
+				if (_rule.priority === null) {
+					_rule.priority = 1;
+					//двигаем приоритет у всех остальных и добавляем их в список измененных
+					if (t.model.aConditionalFormattingRules) {
+						for (i = 0; i < t.model.aConditionalFormattingRules.length; i++) {
+							var _id = t.model.aConditionalFormattingRules[i].id;
+							var oRule = t.model.aConditionalFormattingRules[i].clone();
+							oRule.id = _id;
+							oRule.priority++;
+							arr[nActive].push(oRule);
+						}
+					}
+				}
+				if (_rule.ranges === null) {
+					_rule.ranges = [];
+					if (t.model.selectionRange && t.model.selectionRange.ranges) {
+						for (var j = 0; j < t.model.selectionRange.ranges.length; j++) {
+							_rule.ranges.push(t.model.selectionRange.ranges[j].clone());
+						}
 					}
 				}
 			}
+		};
+
+		var nActive = this.model.workbook.nActive;
+		var i;
+		if (presetId !== undefined) {
+			//data bar/icons/scale presets
+			_checkRule(t.model.generateCFRuleFromPreset(presetId));
+		} else if (arr && arr.length === 1 && undefined === arr[0].length) {
+			//other presets
+			var presetRule = arr[0];
+			arr = [];
+			_checkRule(presetRule);
 		}
 
 		var unitedArr = [];
