6a8dee6ab2b9b5fb60202260bd9fd2211bb1a28d
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-03-24 14:51:01 +0300

    An easier way for ApiHyperlink.SetDefaultStyle() method

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -1951,8 +1951,6 @@
 	{
 		var HyperRun    = null;
 		var ApiHyperRun = null;
-		var Styles = editor.WordControl.m_oLogicDocument.Get_Styles();
-		var oDefaultHyperlinkTextPt = Styles.Get(Styles.GetDefaultHyperlink()).GetTextPr();
 		
 		for (var nRun = 0; nRun < this.ParaHyperlink.Content.length; nRun++)
 		{
@@ -1962,9 +1960,9 @@
 
 			ApiHyperRun = new ApiRun(HyperRun);
 			
-			var oRunTextPr = ApiHyperRun.GetTextPr();
-			oRunTextPr.TextPr.Merge(oDefaultHyperlinkTextPt);
-			oRunTextPr.private_OnChange();
+			HyperRun.Set_Underline(undefined);
+			HyperRun.Set_Color(undefined);
+			HyperRun.Set_Unifill(undefined);
 		}
 			
 		return true;
