e6cc263b5c03b493c6d7d6e663e895d632800125
KirillovIlya <ilya.kirillov@onlyoffice.com>
2021-06-01 21:37:01 +0300

    Fix bug #50604

--- word/Editor/Table.js
+++ word/Editor/Table.js
@@ -16084,8 +16084,7 @@ CTable.prototype.AcceptRevisionChanges = function(nType, bAll)
 			}
 		}
 
-		isAllSelected   = true;
-		isCellSelection = true;
+		isAllSelected = true;
 	}
 
 	if ((bAll || (isCellSelection && !this.ApplyToAll)) && (undefined === nType || c_oAscRevisionsChangeType.TablePr === nType || c_oAscRevisionsChangeType.RowsAdd === nType || c_oAscRevisionsChangeType.RowsRem === nType))
