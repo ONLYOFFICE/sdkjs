51728b199bbe51db015efeb90ec0c4e51294e833
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-06-03 14:34:25 +0300

    Skip checking hidden columns in Api.ReplaceTextSmart()

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -12074,7 +12074,7 @@
 			{
 				for (var nCol = oRange.range.bbox.c1; nCol <= oRange.range.bbox.c2; nCol++)
 				{
-					if (oWorksheet.worksheet.getRowHidden(nRow) || oWorksheet.worksheet.getColHidden(nCol))
+					if (oWorksheet.worksheet.getRowHidden(nRow))
 						continue;
 
 					resultText        = '';
