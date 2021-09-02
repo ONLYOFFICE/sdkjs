9ea965a2f2b274de5c458998aa3fad9d3510b5d0
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2021-09-02 17:36:45 +0300

    Fix minimized version

--- common/apiCommon.js
+++ common/apiCommon.js
@@ -5434,7 +5434,7 @@
 
 		this.buttons = (_object["buttons"] != null) ? _object["buttons"] : this.buttons;
 
-		if (_object["events"] != null) this.set_Events(_object["events"]);
+		if (_object["events"] != null) this["set_Events"](_object["events"]);
 
 		this.size = (_object["size"] != null) ? _object["size"] : this.size;
 		this.initOnSelectionChanged = (_object["initOnSelectionChanged"] != null) ? _object["initOnSelectionChanged"] : this.initOnSelectionChanged;
