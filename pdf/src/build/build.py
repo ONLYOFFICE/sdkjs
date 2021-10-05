8d0be3824e7786a4cb227c519af46b4c37ca0ed6
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2021-10-05 19:19:19 +0300

    build wasm module

--- pdf/src/build/build.py
+++ pdf/src/build/build.py
@@ -14,11 +14,9 @@ base.create_dir("./../deploy")
 base.cmd_in_dir("./../../../../core/DesktopEditor/graphics/pro/js", "python", ["make.py"])
 
 # finalize
-if base.is_exist("./../../../../core/DesktopEditor/graphics/pro/js/deploy/xps_djvu_pdf.wasm"):
-    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/xps.js", "./../deploy/xps.js")
-    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/djvu.js", "./../deploy/djvu.js")
-    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/pdf.js", "./../deploy/pdf.js")
-    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/xps_djvu_pdf.wasm", "./../deploy/xps_djvu_pdf.wasm")
+if base.is_exist("./../../../../core/DesktopEditor/graphics/pro/js/deploy/drawingfile.wasm"):
+    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/drawingfile.js", "./../deploy/drawingfile.js")
+    base.copy_file("./../../../../core/DesktopEditor/graphics/pro/js/deploy/drawingfile.wasm", "./../deploy/drawingfile.wasm")
 else:
     print("xps_djvu_make.py error")
     base.copy_dir("./../pdf", "./../deploy")
