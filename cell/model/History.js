8be0b56a2849c61fae8592106071569cd8ad59e0
GoshaZotov <Igor.Zotov@onlyoffice.com>
2021-10-25 15:04:20 +0300

    [se] Fix tests

--- cell/model/History.js
+++ cell/model/History.js
@@ -1108,7 +1108,8 @@ CHistory.prototype.StartTransaction = function()
 CHistory.prototype.EndTransaction = function()
 {
 	if (1 === this.Transaction && !this.Is_LastPointEmpty()) {
-		var wsView = window["Asc"]["editor"].wb && window["Asc"]["editor"].wb.getWorksheet();
+		var api = this.workbook && this.workbook.oApi;
+		var wsView = api && api.wb && api.wb.getWorksheet();
 		if (wsView) {
 			wsView.updateTopLeftCell();
 		}
