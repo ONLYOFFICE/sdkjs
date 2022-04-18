abc5e689ddc1d6bbea006d3d712c060fa5c561f1
GoshaZotov <Igor.Zotov@onlyoffice.com>
2022-04-18 11:01:21 +0300

    [se] Fix bug 56693

--- cell/api.js
+++ cell/api.js
@@ -1209,7 +1209,7 @@ var editor;
 		if (pages.arrPages.length) {
 			this.asc_drawPrintPreview(0);
 		}
-		return pages.arrPages.length;
+		this.handlers.trigger("asc_onPrintPreviewPagesCountChanged", pages.arrPages.length);
 	};
 
 	spreadsheet_api.prototype.asc_updatePrintPreview = function (options) {
@@ -1217,7 +1217,7 @@ var editor;
 		this.wb.printPreviewState.setPages(pages);
 		this.wb.printPreviewState.setAdvancedOptions(options && options.advancedOptions);
 		var pagesCount = pages.arrPages.length;
-		return pagesCount ? pagesCount : 1;
+		this.handlers.trigger("asc_onPrintPreviewPagesCountChanged", pagesCount ? pagesCount : 1);
 	};
 
 	spreadsheet_api.prototype.asc_drawPrintPreview = function (index, indexSheet) {
