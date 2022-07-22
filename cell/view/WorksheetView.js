75bd29bcc8c753af67db21109aef0ff427bc7a81
GoshaZotov <Igor.Zotov@onlyoffice.com>
2022-07-22 10:41:30 +0300

    [se] Remove unused

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -14999,47 +14999,7 @@
 				callback();
 			}
 		};
-
-
-		var getMultiRanges = function (_ranges, _bCol) {
-			var map = {};
-			var res = [];
-			for (var i = 0; i < _ranges.length; i++) {
-				for (var j = (_bCol ? _ranges[i].c1 : _ranges[i].r1); j <= (_bCol ? _ranges[i].c2 : _ranges[i].r2); j++) {
-					if (!map[j]) {
-						res.push(j);
-						map[j] = 1;
-					}
-				}
-			}
-
-			res = res.sort();
-
-			//объединяем
-			var unionRanges = [];
-			var start = null;
-			var end = null;
-			for (var n = 0; n < res.length; n++) {
-				if (start === null) {
-					start = res[n];
-					end = res[n];
-				} else if (res[n - 1] === res[n] - 1) {
-					end++;
-					if (n === res.length - 1) {
-						unionRanges.push(Asc.Range(_bCol ? start : 0, !_bCol ? start : 0, _bCol ? end : 0, !_bCol ? end : 0));
-					}
-				} else {
-					unionRanges.push(Asc.Range(_bCol ? start : 0, !_bCol ? start : 0, _bCol ? end : 0, !_bCol ? end : 0));
-					start = res[n];
-					end = res[n];
-				}
-			}
-
-			return unionRanges.sort(function(a, b) {
-				return _bCol ? b.c1 - a.c2 : b.r1 - a.r2;
-			});
-		};
-
+		
 		var multiRanges;
 		var minUpdateIndex;
 		var doMultiRanges = function (_func, _start, _end, _byCol) {
