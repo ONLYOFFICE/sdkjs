bd47f6d0e10dd31832fb00f23826f8cb95e77588
GoshaZotov <Igor.Zotov@onlyoffice.com>
2022-10-26 18:32:38 +0300

    [se] Add tests

--- tests/cell/spreadsheet-calculation/FormulaTests.js
+++ tests/cell/spreadsheet-calculation/FormulaTests.js
@@ -5642,23 +5642,23 @@ $(function () {
 		assert.strictEqual(array.getElementRowCol(0, 0).getValue(), 2);
 		assert.strictEqual(array.getElementRowCol(0, 1).getValue(), 'test2');
 		assert.strictEqual(array.getElementRowCol(0, 2).getValue(), '#N/A');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(1, 0).getValue(), 'w');
 		assert.strictEqual(array.getElementRowCol(1, 1).getValue(), 3);
-		
+
 
 		assert.strictEqual(array.getElementRowCol(2, 0).getValue(), 'test');
 		assert.strictEqual(array.getElementRowCol(2, 1).getValue(), 4);
-		
+
 
 		assert.strictEqual(array.getElementRowCol(3, 0).getValue(), 'test11');
 		assert.strictEqual(array.getElementRowCol(3, 1).getValue(), 'test12');
 		assert.strictEqual(array.getElementRowCol(3, 2).getValue(), 'test13');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(4, 0).getValue(), 'test13');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(5, 0).getValue(), 'test14');
 
@@ -5666,31 +5666,31 @@ $(function () {
 
 
 		assert.strictEqual(array.getElementRowCol(7, 0).getValue(), 'test16');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(8, 0).getValue(), 'f');
 		assert.strictEqual(array.getElementRowCol(8, 1).getValue(), 'g');
 		assert.strictEqual(array.getElementRowCol(8, 2).getValue(), 'h');
 		assert.strictEqual(array.getElementRowCol(8, 3).getValue(), 'g');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(9, 0).getValue(), 's');
 		assert.strictEqual(array.getElementRowCol(9, 1).getValue(), '');
 		assert.strictEqual(array.getElementRowCol(9, 2).getValue(), 'd');
 		assert.strictEqual(array.getElementRowCol(9, 3).getValue(), 'f');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(10, 0).getValue(), 'd');
 		assert.strictEqual(array.getElementRowCol(10, 1).getValue(), '');
 		assert.strictEqual(array.getElementRowCol(10, 2).getValue(), 'g');
 		assert.strictEqual(array.getElementRowCol(10, 3).getValue(), 'f');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(11, 0).getValue(), 's');
 		assert.strictEqual(array.getElementRowCol(11, 1).getValue(), 'dfg');
 		assert.strictEqual(array.getElementRowCol(11, 2).getValue(), 's');
 		assert.strictEqual(array.getElementRowCol(11, 3).getValue(), 'd');
-		
+
 
 		assert.strictEqual(array.getElementRowCol(12, 0).getValue(), 'd');
 		assert.strictEqual(array.getElementRowCol(12, 1).getValue(), '');
@@ -5726,6 +5726,89 @@ $(function () {
 
 		//2.8 аргумент - array
 		oParser = new parserFormula("VSTACK({1,2})", "A1", ws);
+		assert.ok(oParser.parse());
+		array = oParser.calculate();
+
+		assert.strictEqual(array.getElementRowCol(0, 0).getValue(), 1);
+		assert.strictEqual(array.getElementRowCol(0, 1).getValue(), 2);
+	});
+
+	QUnit.test("Test: \"HSTACK\"", function (assert) {
+		//1. добавляем общие тесты
+		putStackData();
+		oParser = new parserFormula("HSTACK(A1:B3,B5:D5,B8:B11,B14:E18,A1)", "A1", ws);
+		assert.ok(oParser.parse());
+		let array = oParser.calculate();
+
+		assert.strictEqual(array.getElementRowCol(0, 0).getValue(), 2);
+		assert.strictEqual(array.getElementRowCol(0, 1).getValue(), 'test2');
+		assert.strictEqual(array.getElementRowCol(0, 2).getValue(), 'test11');
+		assert.strictEqual(array.getElementRowCol(0, 3).getValue(), 'test12');
+		assert.strictEqual(array.getElementRowCol(0, 4).getValue(), 'test13');
+		assert.strictEqual(array.getElementRowCol(0, 5).getValue(), 'test13');
+		assert.strictEqual(array.getElementRowCol(0, 6).getValue(), 'f');
+		assert.strictEqual(array.getElementRowCol(0, 7).getValue(), 'g');
+		assert.strictEqual(array.getElementRowCol(0, 8).getValue(), 'h');
+		assert.strictEqual(array.getElementRowCol(0, 9).getValue(), 'g');
+		assert.strictEqual(array.getElementRowCol(0, 10).getValue(), 2);
+
+		assert.strictEqual(array.getElementRowCol(1, 0).getValue(), 'w');
+		assert.strictEqual(array.getElementRowCol(1, 1).getValue(), 3);
+		assert.strictEqual(array.getElementRowCol(1, 5).getValue(), 'test14');
+		assert.strictEqual(array.getElementRowCol(1, 6).getValue(), 's');
+		assert.strictEqual(array.getElementRowCol(1, 7).getValue(), '');
+		assert.strictEqual(array.getElementRowCol(1, 8).getValue(), 'd');
+		assert.strictEqual(array.getElementRowCol(1, 9).getValue(), 'f');
+
+		assert.strictEqual(array.getElementRowCol(2, 0).getValue(), 'test');
+		assert.strictEqual(array.getElementRowCol(2, 1).getValue(), 4);
+		assert.strictEqual(array.getElementRowCol(2, 5).getValue(), '#VALUE!');
+		assert.strictEqual(array.getElementRowCol(2, 6).getValue(), 'd');
+		assert.strictEqual(array.getElementRowCol(2, 7).getValue(), '');
+		assert.strictEqual(array.getElementRowCol(2, 8).getValue(), 'g');
+		assert.strictEqual(array.getElementRowCol(2, 9).getValue(), 'f');
+
+		assert.strictEqual(array.getElementRowCol(3, 5).getValue(), 'test16');
+		assert.strictEqual(array.getElementRowCol(3, 6).getValue(), 's');
+		assert.strictEqual(array.getElementRowCol(3, 7).getValue(), 'dfg');
+		assert.strictEqual(array.getElementRowCol(3, 8).getValue(), 's');
+		assert.strictEqual(array.getElementRowCol(3, 9).getValue(), 'd');
+
+		assert.strictEqual(array.getElementRowCol(4, 6).getValue(), 'd');
+		assert.strictEqual(array.getElementRowCol(4, 7).getValue(), '');
+		assert.strictEqual(array.getElementRowCol(4, 8).getValue(), '');
+		assert.strictEqual(array.getElementRowCol(4, 9).getValue(), 'g');
+
+
+		//2. аргументы - разные типы. нужно пербрать все аргументы
+		//2.1 аргумент - number
+		oParser = new parserFormula("HSTACK(1)", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getElementRowCol(0, 0).getValue(), 1);
+		//2.2 аргумент - string
+		oParser = new parserFormula("HSTACK(\"test\")", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getElementRowCol(0, 0).getValue(), "test");
+		//2.3 аргумент - bool
+		oParser = new parserFormula("HSTACK(true)", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getElementRowCol(0, 0).getValue(), "TRUE");
+		//2.4 аргумент - error
+		oParser = new parserFormula("HSTACK(#VALUE!)", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getValue(), "#VALUE!");
+		//2.5 аргумент - empty
+		oParser = new parserFormula("HSTACK(1,,1)", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getValue(), "#VALUE!");
+		//2.6 аргумент - cellsRange
+		//2.7 аргумент - cell
+		oParser = new parserFormula("HSTACK(B1)", "A1", ws);
+		assert.ok(oParser.parse());
+		assert.strictEqual(oParser.calculate().getElementRowCol(0, 0).getValue(), "test2");
+
+		//2.8 аргумент - array
+		oParser = new parserFormula("HSTACK({1,2})", "A1", ws);
 		assert.ok(oParser.parse());
 		array = oParser.calculate();
 
