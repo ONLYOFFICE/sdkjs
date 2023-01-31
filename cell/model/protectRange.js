9154b14a7fb23a2caea5263b274f8d5c0e8813d4
Igor Zotov <Igor.Zotov@onlyoffice.com>
2023-01-31 13:52:33 +0500

    [se] Added base class by protect user ranges

---
+++ cell/model/protectRange.js
@@ -0,0 +1,218 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2023
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-12 Ernesta Birznieka-Upisha
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
+"use strict";
+
+(function (window) {
+
+	function CUserProtectedRange(/*ws*/) {
+		this.ref = null;
+		this.name = null;
+		this.usersMap  = null;
+
+		//for warning
+		this.warningText = null;
+
+		this.Id = AscCommon.g_oIdCounter.Get_NewId();
+
+		return this;
+	}
+
+	CUserProtectedRange.prototype.Get_Id = function () {
+		return this.Id;
+	};
+
+	CUserProtectedRange.prototype.getType = function () {
+		return AscCommonExcel.UndoRedoDataTypes.ProtectedRangeDataInner;
+	};
+
+	CUserProtectedRange.prototype.clone = function(ws) {
+		var res = new CUserProtectedRange(ws);
+
+		res.ref = this.ref ? this.ref.clone() : null;
+		res.name = this.name;
+		res.usersMap = this.usersMap;
+
+		res.warningText = this.warningText;
+
+		return res;
+	};
+
+	CUserProtectedRange.prototype.Write_ToBinary2 = function(w) {
+		if (null != this.ref) {
+			w.WriteBool(true);
+
+			w.WriteLong(this.ref[i].r1);
+			w.WriteLong(this.ref[i].c1);
+			w.WriteLong(this.ref[i].r2);
+			w.WriteLong(this.ref[i].c2);
+		} else {
+			w.WriteBool(false);
+		}
+		if (null != this.name) {
+			w.WriteBool(true);
+			w.WriteString2(this.name);
+		} else {
+			w.WriteBool(false);
+		}
+
+		if (null != this.usersMap) {
+			let count = 0, i;
+			for (i in this.usersMap) {
+				if (this.usersMap.hasOwnProperty(i)) {
+					count++;
+				}
+			}
+			if (count) {
+				w.WriteBool(true);
+				w.WriteLong(count);
+				for (i in this.usersMap) {
+					if (this.usersMap.hasOwnProperty(i)) {
+						w.WriteString2(i);
+					}
+				}
+			} else {
+				w.WriteBool(false);
+			}
+		} else {
+			w.WriteBool(false);
+		}
+
+		if (null != this.warningText) {
+			w.WriteBool(true);
+			w.WriteString2(this.warningText);
+		} else {
+			w.WriteBool(false);
+		}
+	};
+
+	CUserProtectedRange.prototype.Read_FromBinary2 = function(r) {
+		if (r.GetBool()) {
+			var r1 = r.GetLong();
+			var c1 = r.GetLong();
+			var r2 = r.GetLong();
+			var c2 = r.GetLong();
+			this.sqref = new Asc.Range(c1, r1, c2, r2);
+		}
+		if (r.GetBool()) {
+			this.name = r.GetString2();
+		}
+
+		if (r.GetBool()) {
+			var length = r.GetULong();
+			for (var i = 0; i < length; ++i) {
+				if (!this.usersMap) {
+					this.usersMap = {};
+				}
+				this.usersMap[r.GetString2()] = 1;
+			}
+		}
+
+		if (r.GetBool()) {
+			this.warningText = r.GetString2();
+		}
+	};
+
+	CUserProtectedRange.prototype.asc_getRef = function () {
+		var result = null;
+
+		if (this.ref) {
+			result = this.ref.getAbsName();
+		}
+		return result ? "=" + result : null;
+	};
+	CUserProtectedRange.prototype.asc_getName = function () {
+		return this.name;
+	};
+	CUserProtectedRange.prototype.asc_getUsers = function () {
+		let res = null;
+		if (this.usersMap) {
+			for (let i in this.usersMap) {
+				if (this.usersMap.hasOwnProperty(i)) {
+					if (!res) {
+						res = [];
+					}
+					res.push(i);
+				}
+			}
+		}
+		return res;
+	};
+	CUserProtectedRange.prototype.asc_setRef = function (val) {
+		if (val) {
+			if (val[0] === "=") {
+				val = val.slice(1);
+			}
+			this.ref = [];
+			if (!val) {
+				return;
+			}
+			if (-1 !== val.indexOf("!")) {
+				var is3DRef = AscCommon.parserHelp.parse3DRef(val);
+				if (is3DRef) {
+					val = is3DRef.range;
+				}
+			}
+			this.ref = AscCommonExcel.g_oRangeCache.getAscRange(val);
+		}
+	};
+	CUserProtectedRange.prototype.asc_setName = function (val) {
+		this.name = val;
+	};
+	CUserProtectedRange.prototype.asc_setUsers = function (val) {
+		this.usersMap = {};
+		if (val) {
+			for (let i = 0; i < val.length; i++) {
+				this.usersMap[val[i]] = 1;
+			}
+		}
+	};
+
+
+	//----------------------------------------------------------export----------------------------------------------------
+	var prot;
+	window['Asc'] = window['Asc'] || {};
+	window['AscCommonExcel'] = window['AscCommonExcel'] || {};
+
+	window["Asc"]["CUserProtectedRange"] = window["Asc"].CUserProtectedRange = CUserProtectedRange;
+	prot = CUserProtectedRange.prototype;
+	prot["asc_getRef"] = prot.asc_getRef;
+	prot["asc_getName"] = prot.asc_getName;
+	prot["asc_getUsers"] = prot.asc_getUsers;
+
+	prot["asc_setRef"] = prot.asc_setRef;
+	prot["asc_setName"] = prot.asc_setName;
+	prot["asc_setUsers"] = prot.asc_setUsers;
+
+	prot["asc_getId"] = prot.asc_getId;
+
+})(window);
