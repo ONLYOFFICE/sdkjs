ee36a4894c541c8d6bbd557e921f5bd17b4f91be
Igor Zotov <Igor.Zotov@onlyoffice.com>
2023-05-18 11:46:52 +0500

    [se] Fix

--- cell/model/Serialize.js
+++ cell/model/Serialize.js
@@ -9679,7 +9679,7 @@
 			} else if (c_oSer_SheetView.RightToLeft === type) {
 				this.stream.GetBool();
 			} else if (c_oSer_SheetView.ShowFormulas === type) {
-				this.stream.GetBool();
+				oSheetView.showFormulas = this.stream.GetBool();
 			} else if (c_oSer_SheetView.ShowGridLines === type) {
 				oSheetView.showGridLines = this.stream.GetBool();
 			} else if (c_oSer_SheetView.ShowOutlineSymbols === type) {
@@ -9692,8 +9692,6 @@
 				this.stream.GetBool();
 			} else if (c_oSer_SheetView.ShowZeros === type) {
                 oSheetView.showZeros = this.stream.GetBool();
-			} else if (c_oSer_SheetView.ShowFormulas === type) {
-                oSheetView.showFormulas = this.stream.GetBool();
 			} else if (c_oSer_SheetView.TabSelected === type) {
 				this.stream.GetBool();
 			} else if (c_oSer_SheetView.TopLeftCell === type) {
