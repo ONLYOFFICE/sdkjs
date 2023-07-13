9ba76c0111098a059d8f52c2a0f4c6636b08d824
NikitaKhromov <nikita.khromov@onlyoffice.com>
2023-07-13 19:23:41 +0800

    [bu][de] Set default value of Value of Item in List of CC.

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -16330,7 +16330,7 @@
 	 * Adds the new value to list of values of combobox/listbox.
 	 * @memberof ApiContentControlList
 	 * @param {string} sText - Specifies the display text for the list item..
-	 * @param {string} sValue - Specifies the value of the list item.
+	 * @param {string} [sValue=sText] - Specifies the value of the list item.
 	 * @param {number} [nIndex=this.GetElementsCount()] - position to add.
 	 * @typeofeditors ["CDE"]
 	 * @returns {boolean}
@@ -16341,6 +16341,8 @@
 
 		if (typeof(sText) != "string" || sText == "")
 			return false;
+		if (typeof(sValue) != "string" || sValue == "")
+			sValue = sText;
 		if (typeof(nIndex) != "number" || nIndex < 0 && nIndex > nItemsCount)
 			nIndex = nItemsCount;
 
@@ -16674,7 +16676,7 @@
 	 */
 	ApiContentControlListEntry.prototype.SetValue = function(sValue)
 	{
-		if (typeof(sValue) != "string")
+		if (typeof(sValue) != "string" || sValue == "")
 			return false;
 
 		let isComboBox = this.Sdt.IsComboBox();
