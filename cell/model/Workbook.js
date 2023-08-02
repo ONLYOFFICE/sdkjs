daf2aa52a1dc357784bad395045a254d39a1eebd
Igor Zotov <Igor.Zotov@onlyoffice.com>
2023-08-02 13:26:51 +0500

    [se] Fix bug 63505

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -11475,17 +11475,59 @@
 		let oRange = this.getRange3(range.r1, range.c1, range.r2, range.c2);
 		let res = true;
 
+		let _getLocked = function (_xfs) {
+			let _res = null;
+			if (_xfs && _xfs.applyProtection) {
+				_res = true;//null/true
+				if (_xfs.getLocked() === false) {
+					_res = false;
+				}
+			}
+			return _res;
+		};
+
+		//1. init default value
+		//all cells
 		if (null != this.oAllCol && this.oAllCol.xfs && this.oAllCol.xfs.locked != null) {
-			res = this.oAllCol.xfs.locked;
+			res = _getLocked(this.oAllCol.xfs);
+		}
+
+		//all selected rows
+		let unLockedRowIndex = range.r1 - 1;
+		oRange._foreachRowNoEmpty(function(row){
+			if(null != row && row.xfs && !res === _getLocked(row.xfs)) {
+				if (unLockedRowIndex + 1 === row.index) {
+					unLockedRowIndex++;
+				}
+			}
+		});
+		if (unLockedRowIndex === range.r2) {
+			res = !res;
+		}
+
+		//all selected cols
+		let unLockedColIndex = range.c1 - 1;
+		oRange._foreachColNoEmpty(function(col){
+			if(null != col && col.xfs && !res === _getLocked(col.xfs)) {
+				if (unLockedColIndex + 1 === col.index) {
+					unLockedColIndex++;
+				}
+			}
+		});
+		if (unLockedColIndex === range.c2) {
+			res = !res;
 		}
 
 		oRange._foreachNoEmpty(function(cell){
-			let cellxfs = cell.xfs;
-			let isLocked = cellxfs && cellxfs.asc_getLocked();
-			if (isLocked === null || isLocked === true) {
-				res = isLocked === null ? res : true;
+			if (!cell) {
+				return;
+			}
+
+			var isLocked = _getLocked(cell.xfs);
+			if (isLocked === true) {
+				res = true;
 				return true;
-			} else {
+			} else if (isLocked === false) {
 				res = false;
 			}
 		});
