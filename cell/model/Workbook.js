05651eeb64bcff62a3a8010732054e397eb559fc
Igor Zotov <Igor.Zotov@onlyoffice.com>
2023-08-02 11:17:33 +0500

    [se] Fix bug 63505

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -11472,55 +11472,24 @@
 	};
 
 	Worksheet.prototype.isLockedRange = function (range) {
-		var rangeType = range.getType();
-		var oRange = this.getRange3(range.r1, range.c1, range.r2, range.c2);
-		var res = true;
-		var unLockedRowIndex = range.r1 - 1;
-		oRange._foreachRowNoEmpty(function(row){
-			if(null != row && row.xfs && false === row.xfs.getLocked()) {
-				if (unLockedRowIndex + 1 === row.index) {
-					unLockedRowIndex++;
-				}
-			}
-		});
-		if (unLockedRowIndex === range.r2) {
-			//если все строки разлочены, далее можно не проверять
-			return false;
-		} else if (rangeType === Asc.c_oAscSelectionType.RangeMax || rangeType === Asc.c_oAscSelectionType.RangeRow) {
-			//если выделены все строки, но среди них не все разлочены - далее не проверяем и возвращаем true
-			return true;
+		let oRange = this.getRange3(range.r1, range.c1, range.r2, range.c2);
+		let res = true;
+
+		if (null != this.oAllCol && this.oAllCol.xfs && this.oAllCol.xfs.locked != null) {
+			res = this.oAllCol.xfs.locked;
 		}
 
-		var unLockedColndex = range.c1 - 1;
-		oRange._foreachColNoEmpty(function(col){
-			if(null != col && col.xfs && false === col.xfs.getLocked()) {
-				if (unLockedColndex + 1 === col.index) {
-					unLockedColndex++;
-				}
-			}
-		});
-		if (unLockedColndex === range.c2) {
-			//если все столбцы разлочены, далее можно не проверять
-			return false;
-		} else if (rangeType === Asc.c_oAscSelectionType.RangeMax || rangeType === Asc.c_oAscSelectionType.RangeCol) {
-			//если выделены все столбцы, но среди них не все разлочены - далее не проверяем и возвращаем true
-			return true;
-		}
-
-		oRange._foreach2(function(cell){
-			if (!cell) {
-				res = true;
-				return true;
-			}
-			var cellxfs = cell.xfs;
-			var isLocked = cellxfs && cellxfs.asc_getLocked();
+		oRange._foreachNoEmpty(function(cell){
+			let cellxfs = cell.xfs;
+			let isLocked = cellxfs && cellxfs.asc_getLocked();
 			if (isLocked === null || isLocked === true) {
-				res = true;
+				res = isLocked === null ? res : true;
 				return true;
 			} else {
 				res = false;
 			}
 		});
+
 		return res;
 	};
 
