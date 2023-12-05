509d5584d95473b7a046540073508bef0ee114dc
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-12-05 10:24:32 +0300

    Remove console.log from previous commit.

--- common/apiBase_plugins.js
+++ common/apiBase_plugins.js
@@ -1797,7 +1797,6 @@
 	{
 		let baseUrl = this.pluginsManager.pluginsMap[items.guid].baseUrl;
 		if (items["items"]) correctItemsWithData(items["items"], baseUrl);
-		console.log(items);
 		this.onPluginAddContextMenuItem(items);
 	};
 
