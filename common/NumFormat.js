bea1a93d01e9c355f6dd232a3e9dd51a88610f61
Dmitry Gvozdev <dmitriy.gvozdev@onlyoffice.com>
2024-01-22 15:39:19 +0500

    [se] Fiх bug #65876

--- common/NumFormat.js
+++ common/NumFormat.js
@@ -1650,8 +1650,7 @@ NumFormat.prototype =
 				dayWeek = 3;
 			}
 			// todo test converting all positive fractional values less than one into dates
-			// else if(numberAbs === 0 || (numberAbs > 0 && numberAbs < 1))
-			else if(numberAbs === 0 || (!Number.isInteger(numberAbs) && Math.floor(numberAbs) === 0))
+			else if(numberAbs === 0 || numberAbs > 0 && numberAbs < 1)
 			{
 				//TODO необходимо использовать cDate везде
 				stDate = new Asc.cDate(Date.UTC(1899,11,31,0,0,0));
