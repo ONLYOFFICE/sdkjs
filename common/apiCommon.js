d7857d64a0e026b157ef82372e22b9f0620f69f8
Alexey Matveev <Alexey.Matveev@onlyoffice.com>
2024-02-15 13:09:26 +0300

    [all] Add function for parsing js doc. This is necessary for the cell api builder method "AddCustomFunction".

--- common/apiCommon.js
+++ common/apiCommon.js
@@ -214,6 +214,84 @@
 		return _color;
 	}
 
+	function parseJSDoc(jsDoc) {
+		// function for parsing jsDoc (for builder method "AddCustomFunction")
+		const result = [];
+
+		// A regular expression for searching for JSDoc comments describing parameters, properties, and functions
+		const jsDocRegex = /\/\*\*([\s\S]*?)\*\//g;
+		const paramRegex = /@param\s+{(\??)(.+?)}\s+(?:(\[)?([\w.]+|\{[\w.]+\}))?(?:\s*=\s*([^@]+?)(?=\s|\)|$))?(?:\s+(.+))?/g;
+		const propertyRegex = /@property\s+{(\??)(.+?)}\s+(?:(\[)?([\w.]+|\{[\w.]+\}))?(?:\s*=\s*([^@]+?)(?=\s|\)|$))?(?:\s+(.+))?/g;
+		const returnRegex = /@returns?\s+{(.+?)}\s+(.+)/g;
+	
+		let match;
+		while ((match = jsDocRegex.exec(jsDoc)) !== null) {
+			const parsedData = {};
+			const commentBlock = match[1].trim();
+	
+			// Parsing of function parameters
+			const params = [];
+			let paramMatch;
+			while ((paramMatch = paramRegex.exec(commentBlock)) !== null) {
+				const type = paramMatch[2];
+				const isOptional = paramMatch[3] !== undefined || paramMatch[1] === '?';
+				const name = paramMatch[4];
+				const defaultValue = isOptional && paramMatch[5] !== undefined ? paramMatch[5].trim().replace(/\]$/, '') : undefined;
+				const description = paramMatch[6] || '';
+	
+				params.push({
+					type,
+					name,
+					isOptional,
+					defaultValue,
+					description
+				});
+			}
+	
+			// Parsing properties
+			const properties = [];
+			let propertyMatch;
+			while ((propertyMatch = propertyRegex.exec(commentBlock)) !== null) {
+				const type = propertyMatch[2];
+				const isOptional = propertyMatch[3] !== undefined || propertyMatch[1] === '?';
+				const name = propertyMatch[4];
+				const defaultValue = isOptional && propertyMatch[5] !== undefined ? propertyMatch[5].trim().replace(/\]$/, '') : undefined;
+				const description = propertyMatch[6] || '';
+	
+				properties.push({
+					type,
+					name,
+					isOptional,
+					defaultValue,
+					description
+				});
+			}
+	
+			// Parsing what the function returns
+			let returnInfo = null;
+			const returnMatch = returnRegex.exec(commentBlock);
+			if (returnMatch !== null) {
+				returnInfo = {
+					type: returnMatch[1],
+					description: returnMatch[2]
+				};
+			}
+	
+			// Parsing function description
+			const descriptionRegex = /\*\s*(.*)/g;
+			const descriptionMatch = descriptionRegex.exec(commentBlock);
+			const description = descriptionMatch ? descriptionMatch[1].trim() : '';
+	
+			parsedData.params = params;
+			parsedData.properties = properties;
+			parsedData.returnInfo = returnInfo;
+			parsedData.description = description;
+			result.push(parsedData);
+		}
+	
+		return result;
+	};
+
 	var c_oLicenseResult = {
 		Error         : 1,
 		Expired       : 2,
@@ -8073,6 +8151,7 @@
     window["AscCommon"].isValidJs = isValidJs;
     window["AscCommon"].asc_menu_ReadPaddings = asc_menu_ReadPaddings;
     window["AscCommon"].asc_menu_ReadColor = asc_menu_ReadColor;
+    window["AscCommon"].parseJSDoc = parseJSDoc;
 
 	window["Asc"]["PluginType"] = window["Asc"].PluginType = PluginType;
 	window["Asc"]["CPluginVariation"] = window["Asc"].CPluginVariation = CPluginVariation;
