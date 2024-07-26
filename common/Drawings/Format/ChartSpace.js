26252eb4425fa2e6fcda87432b6e262181263697
ansaraidarbek <ansar.aydarbek@onlyoffice.com>
2024-07-26 16:20:54 +0500

    [all] Trendlines forward/backward fixed and labels are now displayed correctly

--- common/Drawings/Format/ChartSpace.js
+++ common/Drawings/Format/ChartSpace.js
@@ -4783,7 +4783,9 @@ function(window, undefined) {
 								oCurPts = oSeries.val.numLit;
 							}
 							if (oCurPts) {
-								nPtsLength = Math.max(nPtsLength, oCurPts.ptCount);
+								const forward = oSeries.trendline && oSeries.trendline.forward ? oSeries.trendline.forward : 0;
+								const newNPtsLength = oCurPts.ptCount + forward;
+								nPtsLength = Math.max(nPtsLength, newNPtsLength);
 							}
 						}
 					}
