11127972ee2252d62a5bca711de5ca8535d45a75
Aleksandr Nagaev <aleksandr.nagaev@onlyoffice.com>
2026-01-26 17:47:37 +0300

    [SE] optimize sumif func (part). Small refactor COUNTIF methods

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -1677,6 +1677,7 @@
 			AscCommonExcel.g_oSUMIFSCache.clean();
 			AscCommonExcel.g_oFormulaRangesCache.clean();
 			AscCommonExcel.g_oCountIfCache.clean();
+			AscCommonExcel.g_oSumIfCache.clean();
 		},
 		notifyAllFormulasInChangedWs: function() {
 			let sheetIds = {};
@@ -17294,6 +17295,7 @@
 			AscCommonExcel.g_oSUMIFSCache.remove(this);
 			AscCommonExcel.g_oFormulaRangesCache.remove(this);
 			AscCommonExcel.g_oCountIfCache.remove(this, DataOld, res);
+			AscCommonExcel.g_oSumIfCache.remove(this, DataOld, res);
 		}
 	};
 	Cell.prototype.cleanText = function() {
