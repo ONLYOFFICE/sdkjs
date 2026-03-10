44b8a648a7a8a50cb7a268dc3646b57c0d4e7562
Angsar <ansar.aydarbek@onlyoffice.com>
2026-03-10 11:56:50 +0500

    [all] Fix bug 70945

--- common/Charts/ChartsDrawer.js
+++ common/Charts/ChartsDrawer.js
@@ -2899,8 +2899,8 @@ CChartsDrawer.prototype =
             val : {max: null, min:null, scale : []},
         }
 
-		// for (let i = 0; i < plotArea.plotAreaRegion.series.length; i++) {
-            const seria = plotArea.plotAreaRegion.series[0];
+		for (let i = 0; i < plotArea.plotAreaRegion.series.length; i++) {
+            const seria = plotArea.plotAreaRegion.series[i];
             const type = seria.layoutId;
             const secondType = 1 < plotArea.plotAreaRegion.series.length ? plotArea.plotAreaRegion.series[1].layoutId : null;
             const strLit = seria.getCatLit(type);
@@ -2931,7 +2931,7 @@ CChartsDrawer.prototype =
                 plotArea.plotAreaRegion.setCachedData(this._createCachedData(type, seria, numLit, strLit, axisProperties, secondType, width, height));
 				axisProperties.isValid = true;
             }
-        // }
+        }
 
 		if (axisProperties.isValid) {
 			this._chartExHandleAxesConfigurations(plotArea.axId, axisProperties);
