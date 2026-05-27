900769b427946edef9f248dba5682517d3320b5a
samirInnovx <samir@innovxcare.ai>
2026-05-27 12:11:37 +0530

    handling break on preload

--- word/api.js
+++ word/api.js
@@ -1540,12 +1540,14 @@ background-repeat: no-repeat;\
     };
 
   function BeforeOpenDocument() {
+    AscCommon.History.TurnOff();
     this.InitEditor();
     this.DocumentType = 2;
     this.LoadedObjectDS = this.WordControl.m_oLogicDocument.CopyStyle();
 
     g_oIdCounter.Set_Load(true);
     AscFonts.IsCheckSymbols = true;
+    AscCommon.History.TurnOn();
   }
   function AfterOpenDocument(data, size) {
     if (History && History.Update_FileDescription)
